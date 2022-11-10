b62290386e3e73198d8d5b42a02da2177b0495fe
ekmvw3v093 <117622084+ekmvw3v093@users.noreply.github.com>
2022-11-10 15:12:06 +0800

    APEX

---
+++ KG1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public Transform plane;
+    //private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(plane);
+    }
+}
---
+++ KG1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float upSpeed;
+    public float rightSpeed;
+    public float rotationSpeed;
+    public float verticalInput;
+    public float horizontalInput;
+
+   
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.up * upSpeed * verticalInput);
+        transform.Translate(Vector3.right * rightSpeed * horizontalInput);
+        transform.Translate(Vector3.forward * speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(0.01f , 0, 0);
+        transform.Rotate(Input.GetAxis("Vertical") * rotationSpeed * -1, 0, 0);
+        transform.Rotate(Input.GetAxis("Horizontal") * rotationSpeed * -1, 0, 0);
+    }
+}
